dcbfb92dad8f6b915ac58c1e5e05f0602d91cb35
lennartCln <lennart.clausen@gmail.com>
2020-10-26 16:05:05 +0100

    bugfix in deployment

--- deployment/setup_lambda.py
+++ deployment/setup_lambda.py
@@ -1,7 +1,7 @@
 import boto3
 import os
 import io
-import zipfile
+from zipfile import ZipFile
 
 import config as C
 
@@ -9,26 +9,22 @@ client = boto3.client('lambda')
 
 def files_to_zip(path):
     for root, dirs, files in os.walk(path):
-        for f in files:
-            full_path = os.path.join(root, f)
-            print(full_path)
-            archive_name = full_path[len(path) + len(os.sep):]
-            yield full_path, archive_name
+        for file_name in files:
+            full_path = os.path.join(root, file_name)
+            yield full_path, file_name
 
 def make_zip_file_bytes(path):
     buf = io.BytesIO()
-    with zipfile.ZipFile(buf, 'w') as z:
+    with ZipFile(buf, 'w') as z:
         for full_path, archive_name in files_to_zip(path=path):
             z.write(full_path, archive_name)
     return buf.getvalue()
 
 if __name__ == '__main__':
     lambda_code = '../lambda_function/'
-
     response = client.update_function_code(
         FunctionName= C.AWS['function_name'],
         ZipFile= make_zip_file_bytes(lambda_code))
-
     print(response)
 
 
